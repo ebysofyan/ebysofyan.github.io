624c57d967cf08a302435406742f41826a48a961
ebysofyan <eby.sofyan@gmail.com>
2020-07-21 23:02:57 +0800

    fix invitations page

--- nwedding/data/invitations.js
+++ nwedding/data/invitations.js
@@ -1,59 +1,296 @@
 var invitations = [
-  { id: '72fbe93a-b352-11e8-96f8-529269fb1459', name: 'ITEC Group' },
-  { id: '72fc6f7c-b352-11e8-96f8-529269fb1459', name: 'Tim Juara' },
-  { id: '72fc71c0-b352-11e8-96f8-529269fb1459', name: 'Tim Lumbung Inovasi' },
-  { id: '72fc7404-b352-11e8-96f8-529269fb1459', name: 'Tim LobarApps' },
-  { id: '72fc765c-b352-11e8-96f8-529269fb1459', name: 'Tim Kholas Ceria' },
-  { id: '72fc7896-b352-11e8-96f8-529269fb1459', name: 'Bang Firman' },
-  { id: '72fc7ae4-b352-11e8-96f8-529269fb1459', name: 'Akh Robi Gagah' },
-  { id: '72fc8048-b352-11e8-96f8-529269fb1459', name: 'Orang Yang Diundang' },
-  { id: '72fc82b4-b352-11e8-96f8-529269fb1459', name: 'Bang Tatang' },
-  { id: '72fc8502-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc873c-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc8976-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc8bba-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc92f4-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc957e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc97b8-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc99e8-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc9c18-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fc9e2a-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fca320-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fca58c-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fca7da-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcaa14-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcac4e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcae7e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcb0ae-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcb540-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcb798-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcba36-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcbc8e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcbf18-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcc490-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcc6fc-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcc940-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fccb7a-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fccdaa-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fccfda-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcd21e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcd746-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcd994-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fce33a-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fce81c-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fcea7e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd159e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd1ae4-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd1d46-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd1f80-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd21ba-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd2412-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd264c-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd2c0a-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd2ec6-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd311e-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd336c-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd35ba-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd37ea-b352-11e8-96f8-529269fb1459', name: '' },
-  { id: '72fd3d08-b352-11e8-96f8-529269fb1459', name: '' },
+  {
+    id: "72fbe93a-b352-11e8-96f8-529269fb1459",
+    name: "ITEC Group"
+  },
+  {
+    id: "72fc6f7c-b352-11e8-96f8-529269fb1459",
+    name: "Tim Juara"
+  },
+  {
+    id: "72fc71c0-b352-11e8-96f8-529269fb1459",
+    name: "Tim Lumbung Inovasi"
+  },
+  {
+    id: "72fc7404-b352-11e8-96f8-529269fb1459",
+    name: "Tim LobarApps"
+  },
+  {
+    id: "72fc765c-b352-11e8-96f8-529269fb1459",
+    name: "Tim Kholas Ceria"
+  },
+  {
+    id: "72fc7896-b352-11e8-96f8-529269fb1459",
+    name: "Bang Firman"
+  },
+  {
+    id: "72fc7ae4-b352-11e8-96f8-529269fb1459",
+    name: "Akh Robi Huzwandar"
+  },
+  {
+    id: "72fc8048-b352-11e8-96f8-529269fb1459",
+    name: "Orang Yang Diundang"
+  },
+  {
+    id: "72fc82b4-b352-11e8-96f8-529269fb1459",
+    name: "Pak Tatang & Istri"
+  },
+  {
+    id: "72fc9c18-b352-11e8-96f8-529269fb1459",
+    name: "Dayat & Ela"
+  },
+  {
+    id: "72fc9e2a-b352-11e8-96f8-529269fb1459",
+    name: "Fawaz & Ana"
+  },
+  {
+    id: "72fca320-b352-11e8-96f8-529269fb1459",
+    name: "Bang Arief & Istri"
+  },
+  {
+    id: "72fca58c-b352-11e8-96f8-529269fb1459",
+    name: "Damu Archery"
+  },
+  {
+    id: "72fca7da-b352-11e8-96f8-529269fb1459",
+    name: "Bang Kahfi"
+  },
+  {
+    id: "72fcaa14-b352-11e8-96f8-529269fb1459",
+    name: "Kelompok Bujang Sholeh"
+  },
+  {
+    id: "72fcac4e-b352-11e8-96f8-529269fb1459",
+    name: "Akh Endra Faelani"
+  },
+  // My Father's Invited Friend
+  {
+    id: "72fcae7e-b352-11e8-96f8-529269fb1459",
+    name: "Bpk. Nurdin Kasim & Istri"
+  },
+  {
+    id: "72fcb0ae-b352-11e8-96f8-529269fb1459",
+    name: "Bpk. Sahrun & Istri"
+  },
+  {
+    id: "72fcb540-b352-11e8-96f8-529269fb1459",
+    name: "Bpk. Fredy & Istri"
+  },
+  {
+    id: "72fcb798-b352-11e8-96f8-529269fb1459",
+    name: "Uki & Istri"
+  },
+  {
+    id: "72fcba36-b352-11e8-96f8-529269fb1459",
+    name: "Ari & Hendra"
+  },
+  {
+    id: "72fcbc8e-b352-11e8-96f8-529269fb1459",
+    name: "Pak Edy & Istri"
+  },
+  {
+    id: "72fcbf18-b352-11e8-96f8-529269fb1459",
+    name: "Zaenul Halidi"
+  },
+  {
+    id: "72fcc490-b352-11e8-96f8-529269fb1459",
+    name: "Dedy & Istri"
+  },
+  {
+    id: "72fcc6fc-b352-11e8-96f8-529269fb1459",
+    name: "Oby"
+  },
+  {
+    id: "72fcc940-b352-11e8-96f8-529269fb1459",
+    name: "Bpk. Minta Sri Hardi & Istri"
+  },
+  {
+    id: "72fccb7a-b352-11e8-96f8-529269fb1459",
+    name: "Lalu Hery"
+  },
+  {
+    id: "38bedd18-9cf2-41f4-8acb-3dea7179a747",
+    name: "Pak Ali"
+  },
+  // Ulfana's Invited Friends
+  {
+    id: "72fc8502-b352-11e8-96f8-529269fb1459",
+    name: "Novia Fuji Ayu Trisnawati"
+  },
+  {
+    id: "72fc873c-b352-11e8-96f8-529269fb1459",
+    name: "M. Rozali"
+  },
+  {
+    id: "72fc8976-b352-11e8-96f8-529269fb1459",
+    name: "Alumni Kesehatan Hewan 2017"
+  },
+  {
+    id: "72fc8bba-b352-11e8-96f8-529269fb1459",
+    name: "Alumni SDN Lendang Ara 2008"
+  },
+  {
+    id: "72fc92f4-b352-11e8-96f8-529269fb1459",
+    name: "Tim Admin"
+  },
+  {
+    id: "72fc957e-b352-11e8-96f8-529269fb1459",
+    name: "Sahabat Kecil"
+  },
+  {
+    id: "72fccdaa-b352-11e8-96f8-529269fb1459",
+    name: "Rauhil"
+  },
+  {
+    id: "72fccfda-b352-11e8-96f8-529269fb1459",
+    name: "Baiq Dwi"
+  },
+  {
+    id: "72fcd21e-b352-11e8-96f8-529269fb1459",
+    name: "Herman & Hasmi"
+  },
+  {
+    id: "72fcd746-b352-11e8-96f8-529269fb1459",
+    name: "Bunda Rinti Hayati"
+  },
+  {
+    id: "72fcd994-b352-11e8-96f8-529269fb1459",
+    name: "Bunda Titi"
+  },
+  {
+    id: "72fce33a-b352-11e8-96f8-529269fb1459",
+    name: "Mama Baiq Adnin (Nink)"
+  },
+  {
+    id: "72fce81c-b352-11e8-96f8-529269fb1459",
+    name: "Ibu Mar'atus Sholihah (Ika)"
+  },
+  {
+    id: "72fcea7e-b352-11e8-96f8-529269fb1459",
+    name: "Mba Tita"
+  },
+  {
+    id: "72fd159e-b352-11e8-96f8-529269fb1459",
+    name: "Drg Rina Yuliandari"
+  },
+  {
+    id: "72fd1ae4-b352-11e8-96f8-529269fb1459",
+    name: "Faidza Shodiqoh"
+  },
+  {
+    id: "72fd1d46-b352-11e8-96f8-529269fb1459",
+    name: "Lau Riyan Arya Pradana"
+  },
+  {
+    id: "72fd1f80-b352-11e8-96f8-529269fb1459",
+    name: "Devia Wulandasari"
+  },
+  {
+    id: "72fd21ba-b352-11e8-96f8-529269fb1459",
+    name: "Bq Busniati Zuraida"
+  },
+  {
+    id: "72fd2412-b352-11e8-96f8-529269fb1459",
+    name: "Mba Ema"
+  },
+  {
+    id: "72fd264c-b352-11e8-96f8-529269fb1459",
+    name: "Nadia Afidati"
+  },
+  {
+    id: "72fd2c0a-b352-11e8-96f8-529269fb1459",
+    name: "Om Hery & Tante Diana"
+  },
+  {
+    id: "72fd2ec6-b352-11e8-96f8-529269fb1459",
+    name: "Hafiza"
+  },
+  {
+    id: "72fd311e-b352-11e8-96f8-529269fb1459",
+    name: "Novia Dwi Putri"
+  },
+  {
+    id: "72fd336c-b352-11e8-96f8-529269fb1459",
+    name: "Nurul Wahidah"
+  },
+  {
+    id: "72fd35ba-b352-11e8-96f8-529269fb1459",
+    name: "Hamdalah"
+  },
+  {
+    id: "72fd37ea-b352-11e8-96f8-529269fb1459",
+    name: "Resqi Maulinda"
+  },
+  {
+    id: "72fd3d08-b352-11e8-96f8-529269fb1459",
+    name: "Mba Dahlia"
+  },
+  {
+    id: "ba18e33a-d497-48d6-8feb-5b621501a602",
+    name: "Member Grup Ngopi Santuy"
+  },
+  {
+    id: "9f49883b-166a-482c-8e79-81bd0bc6d643",
+    name: "Ria Suryana"
+  },
+  {
+    id: "feb10460-6138-4064-ad37-93f614356bdd",
+    name: "Nenang Hartati"
+  },
+  {
+    id: "4e3e0c81-677a-40c3-b6a2-953a00d21e0a",
+    name: "Yuliana Farida"
+  },
+  {
+    id: "53a2087e-c600-4768-b8f7-f4b29067973b",
+    name: "Yusri Rahman"
+  },
+  {
+    id: "1b218122-9768-4069-81fd-05220b84e2c6",
+    name: "Hasmi Layang Sari"
+  },
+  {
+    id: "3cd7d8fd-4119-4721-bb4f-1aa83e3c8b70",
+    name: "Mia Agustina"
+  },
+  {
+    id: "5cf7e71d-7a2e-4079-b288-54b013ea7c8c",
+    name: "Rini Jati Ayu"
+  },
+  {
+    id: "f1211b33-5a06-4866-8654-1e5e977f27f4",
+    name: "Dr Akhda Maulana Sp.U"
+  },
+  {
+    id: "bf7fbb81-e1d4-4c5a-a9a5-dd4e12876f72",
+    name: "Zahratul Aeny"
+  },
+  {
+    id: "b0e49d49-20f5-43d5-a6d7-526a57da48d6",
+    name: "Anggota Group Ini"
+  },
+  {
+    id: "ea12c262-2104-46e8-9452-043b3420a253",
+    name: "Mba Ida"
+  },
+  {
+    id: "302956a9-7e79-43cc-b175-3dda7782a625",
+    name: "Mba Hj. Rahmi"
+  },
+  {
+    id: "1592487f-2966-48ad-b680-1c6017b503e4",
+    name: "Bunda Tuti"
+  },
+  {
+    id: "72fc97b8-b352-11e8-96f8-529269fb1459",
+    name: "Sister Fillah"
+  },
+  {
+    id: "09a2015e-9250-403d-9c7c-87ee8e101b45",
+    name: "Bq Meilinda Kurnia A."
+  },
+  {
+    id: "fec1627b-87b8-4c71-bb68-0092bc9a8bdb",
+    name: "Heri Khazwardi"
+  },
 ]
